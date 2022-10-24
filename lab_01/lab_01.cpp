16771badd34fd42ebbd04a9a59ed391db2cad476
Oleksandr <114135649+Qwedcy@users.noreply.github.com>
2022-10-24 11:52:36 +0300

    test

---
+++ lab_01/lab_01.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string input;
+    cout << "Введи будь-яке слово\n";
+    cin >> input;
+    cout << "Твоє слово: " + input;
+}
